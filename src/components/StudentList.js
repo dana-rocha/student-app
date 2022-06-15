7870fad2eb85862e6875cfa5170fd62e5d7d8c00
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-15 15:02:11 -0700

    Iterating over data

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -4,11 +4,29 @@ import Student from './Student';
 
 const StudentList = () => {
 
-    const studentComponents = [
-        <li><Student name="Ada" email="ada@dev.org"></Student></li>,
-        <li><Student name="Soo-ah" email="sooah@dev.org"></Student></li>,
-        <li><Student name="Chrissy" email="chrissy@dev.org"></Student></li>
+    const studentData = [
+        {
+            nameData: 'Ada',
+            emailData: 'ada@dev.org'
+        },
+        {
+            nameData: 'Soo-ah',
+            emailData: 'sooah@dev.org'
+        },
+        {
+            nameData: 'Chrissy',
+            emailData: 'chrissy@dev.org'
+        }
     ];
+
+
+    const studentComponents = studentData.map(student => {
+        return (
+            <li><Student name={student.nameData} email={student.emailData}></Student></li>
+        );
+
+    });
+
 
     return (
         <section>
