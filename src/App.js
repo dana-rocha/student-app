01fe259a1a56a25054ead3315f2cd6b00be8a4a6
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-14 17:46:58 -0700

    Adding a second component ClassInfo

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
   return (
     <main>
       <h1>Attendance</h1>
-      Here's the first rendered student list:
-      <StudentList></StudentList>
-      Here's the second rendered student list:
+      <ClassInfo></ClassInfo>
       <StudentList></StudentList>
     </main>
   );
