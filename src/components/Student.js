19a86e9f90827dac334838a9d09936cc995f1b08
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-17 09:50:27 -0700

    Lifted State up

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 import './Student.css';
 
 const Student = (props) => {
+
+    const onAttendanceButtonClick = () => {
+        const updatedStudent = {
+            id: props.id,
+            nameData: props.name,
+            emailData: props.email,
+            isPresentData: !props.isPresent
+        };
+
+        // Invoke the function passed in through the prop named "onUpdate"
+        // This function is referenced by the name "updateStudentData" in App
+        props.onUpdate(updatedStudent);
+    };
 
     const nameColor = props.isPresent ? 'green' : 'red';
 
     return (
         <div>
-        <ul>
-            <li className={nameColor}>Nickname: {props.name}</li>
-            <li>Email: {props.email}</li>
-        </ul>
-        <button onClick={/* togglePresence */}>Toggle if {props.name} is present</button>        </div>
+            <ul>
+                <li className={nameColor}>Nickname: {props.name}</li>
+                <li>Email: {props.email}</li>
+            </ul>
+            <button onClick={onAttendanceButtonClick}>Toggle if {props.name} is present</button>        
+        </div>
     );
 };
 
@@ -21,7 +34,8 @@ Student.propTypes = {
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     email: PropTypes.string.isRequired,
-    isPresent: PropTypes.bool
+    isPresent: PropTypes.bool,
+    onUpdate: PropTypes.func
 };
 
 export default Student;
