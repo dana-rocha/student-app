17d8210823cf1567b3554f5451da4e9c50af4ab6
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-16 17:49:33 -0700

    Adding State in Student.js

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const Student = (props) => {
+    const [isPresent, setIsPresent] = useState(false);
+
     return (
+        <div>
         <ul>
             <li>Nickname: {props.name}</li>
             <li>Email: {props.email}</li>
         </ul>
+        <button>Toggle if {props.name} is present</button>
+        </div>
     );
 };
 
