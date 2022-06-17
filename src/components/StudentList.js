c7adef43fe7ac3737f6ab7149dec1a76edab4711
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-17 08:05:15 -0700

    State

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import './StudentList.css'
+import './StudentList.css';
 import Student from './Student';
 import PropTypes from 'prop-types';
 
 const StudentList = (props) => {
 
-    const studentComponents = props.students.map(student => {
-        return (
-            <li><Student name={student.nameData} email={student.emailData}></Student></li>
-        );
-
+    const studentComponents = props.students.map((student) => {
+    return (
+        <li>
+        <Student name={student.nameData} email={student.emailData}></Student>
+        </li>
+    );
     });
 
-
     return (
-        <section>
-            <h2>Student List</h2>
-            <ul> {studentComponents}</ul>
-        </section>
+    <section>
+        <h2>Student List</h2>
+        <ul> {studentComponents}</ul>
+    </section>
     );
 };
 
 StudentList.propTypes = {
     students: PropTypes.arrayOf(
-        PropTypes.shape({
-            nameData: PropTypes.string.isRequired,
-            emailData: PropTypes.string.isRequired,
-        })
+    PropTypes.shape({
+        nameData: PropTypes.string.isRequired,
+        emailData: PropTypes.string.isRequired,
+    })
     ),
 };
 
