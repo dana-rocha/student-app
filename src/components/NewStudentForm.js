b80a232a1ced3139f2f13ad866328814e932138b
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-22 23:22:58 -0700

    New component for student form

---
+++ src/components/NewStudentForm.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+
+// New piece of state to hold the form data and input values read from state
+// Creating an event handler for each input field
+// Each event handler will update state
+
+const NewStudentForm = () => {
+
+    const [formFields, setFormFields] = useState({
+        name: '',
+        email: ''
+    });
+
+    // One event handler to read current value inside the input field
+    // Another event handler to update state to that current value
+
+    const onNameChange = (event) => {
+        setFormFields({
+            ...formFields,
+            name: event.target.value
+        })
+    };
+
+    const onEmailChange = (event) => {
+        setFormFields({
+            ...formFields,
+            email: event.target.value
+        })
+    };
+
+    return (
+        <form>
+            <div>
+                <label htmlFor="fullName"> Name: </label>
+                <input 
+                    name="fullName"
+                    value={formFields.name}/>
+            </div>
+            <div>
+                <label htmlFor="email"> Email: </label>
+                <input 
+                    name="email"
+                    value={formFields.email}/>
+            </div>
+            <input
+                type="submit"
+                value="Add Student" 
+            />
+        </form>
+    );
+};
+
+export default NewStudentForm;
