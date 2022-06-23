8d8a0b9dd4d320687062be9715dce906e28d92c1
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-23 09:30:41 -0700

    Passing down New Student Handlers

--- src/App.js
+++ src/App.js
@@ -39,6 +39,23 @@ function App() {
     setStudentData(students);
   };
 
+  const addStudentData = newStudent => {
+    // Duplicate the student list
+    const newStudentList = [...studentData];
+
+    // Logic to generate the next valid student ID
+    const nextId = Math.max(...newStudentList.map(student => student.id)) + 1;
+
+    newStudentList.push({
+      id: nextId,
+      nameData: newStudent.nameData,
+      emailData: newStudent.emailData,
+      isPresentData: false,
+    });
+
+    setStudentData(newStudentList);
+  };
+
   return (
     <main>
       <h1>Attendance</h1>
